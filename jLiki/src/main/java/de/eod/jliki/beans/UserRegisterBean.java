c6afc61583ce8224e7ccf7fe8e316784dff27f05
Sebastian Maisch <sebastian.maisch@googlemail.com>
2011-11-09 11:04:22 +0100

    Updated licenses, removed unlicensed files.

--- jLiki/src/main/java/de/eod/jliki/beans/UserRegisterBean.java
+++ jLiki/src/main/java/de/eod/jliki/beans/UserRegisterBean.java
@@ -1,11 +1,21 @@
 /**
- * Copyright (C) 2011 The jLiki Programming Team.
+ * /**
+ * Copyright (C) ${year} The jLiki Programming Team.
  *
  * This file is part of jLiki.
  *
+ * jLiki is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * jLiki is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jLiki.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Author: Sebastian Maisch
  * Last changes:
